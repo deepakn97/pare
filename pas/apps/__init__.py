13da04df4237755fccf54feecc128fab024d4cbc
JasonZ-c <zcheng@mail.ustc.edu.cn>
2025-12-16 23:35:04 -0800

    clean apps init

--- pas/apps/__init__.py
+++ pas/apps/__init__.py
@@ -4,12 +4,15 @@ from __future__ import annotations
 
 from are.simulation.apps import App
 
+from pas.apps.apartment import StatefulApartmentApp
+from pas.apps.cab import StatefulCabApp
 from pas.apps.calendar.app import StatefulCalendarApp
 from pas.apps.contacts.app import StatefulContactsApp
 from pas.apps.core import AppState, StatefulApp
 from pas.apps.email.app import StatefulEmailApp
 from pas.apps.messaging.app import StatefulMessagingApp
 from pas.apps.proactive_aui import PASAgentUserInterface
+from pas.apps.shopping import StatefulShoppingApp
 from pas.apps.system import HomeScreenSystemApp
 from pas.apps.tool_decorators import user_tool
 
@@ -18,11 +21,14 @@ __all__ = [
     "AppState",
     "HomeScreenSystemApp",
     "PASAgentUserInterface",
+    "StatefulApartmentApp",
     "StatefulApp",
+    "StatefulCabApp",
     "StatefulCalendarApp",
     "StatefulContactsApp",
     "StatefulEmailApp",
     "StatefulMessagingApp",
+    "StatefulShoppingApp",
     "user_tool",
 ]
 
@@ -32,5 +38,8 @@ ALL_APPS = [
     StatefulCalendarApp,
     StatefulContactsApp,
     StatefulEmailApp,
+    StatefulShoppingApp,
+    StatefulCabApp,
+    StatefulApartmentApp,
     StatefulMessagingApp,
 ]
